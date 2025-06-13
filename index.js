efa1f440dc7aa56cc7836c743866a13cbdac7772
arnxv-coder <playnav.yt@gmail.com>
2025-06-14 04:08:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const { Client } = require("discord.js-selfbot-v13");
+const express = require("express");
 
 const client = new Client();
+const app = express();
+
+app.get("/", (_, res) => res.send("Selfbot alive!"));
+app.listen(3000, () => console.log("Web server running"));
 
 client.on("ready", () => {
   console.log(`✅ Logged in as ${client.user.username}`);
