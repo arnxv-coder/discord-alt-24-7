f83efd5bc9952c43347affcacf97ae2fb8148adc
arnxv-coder <playnav.yt@gmail.com>
2025-06-14 04:16:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { Client } = require("discord.js-selfbot-v13");
 const express = require("express");
 
-const client = new Client();
 const app = express();
-
-app.get("/", (_, res) => res.send("Selfbot alive!"));
+app.get("/", (_, res) => res.send("Bot is running"));
 app.listen(3000, () => console.log("Web server running"));
+
+process.env.DISCORD_SELFBOT_DISABLE_SETTINGS_PATCH = "true"; // 👈 Add this line
+
+const client = new Client();
 
 client.on("ready", () => {
   console.log(`✅ Logged in as ${client.user.username}`);
